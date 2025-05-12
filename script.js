31992ef309b60ccde37c1597feb9afc5ff07d6e1
dauntflash <ndwiga14053@student.mmarau.ac.ke>
2025-05-12 11:50:58 +0300

    Add error handling and UI enhancements for password generation

--- script.js
+++ script.js
@@ -4,63 +4,67 @@ const uppercase = document.getElementById("uppercase");
 const lowercase = document.getElementById("lowercase");
 const specialcharacters = document.getElementById("specialcharacters");
 const copy_button = document.getElementById("copy");
-const length_number= document.getElementById("number-length");
+const length_number = document.getElementById("number-length");
 
 const lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
 const uppercase_letters = lowercase_letters.toUpperCase();
 const symbols = "~!`@#$%^&*_-=+/?><.,|";
 
+const error = document.getElementById("error");
+const messageBox = document.getElementById("message");
+
 function generate_password() {
-    let password_array = [];
+  let password_array = [];
 
-    const useUppercase = uppercase.checked;
-    const useLowercase = lowercase.checked;
-    const useSymbols = specialcharacters.checked;
+  const useUppercase = uppercase.checked;
+  const useLowercase = lowercase.checked;
+  const useSymbols = specialcharacters.checked;
 
-    if (!useUppercase && !useLowercase && !useSymbols) {
-        for (let i = 0; i < length_number.value; i++) {
-            const number = Math.floor(Math.random() * 10);
-            password_array.push(number);
-        }
-    } else {
-        if (useUppercase) {
-            const uppercase_idx = Math.floor(Math.random() * uppercase_letters.length);
-            password_array.push(uppercase_letters[uppercase_idx]);
-        }
-        if (useLowercase) {
-            const lowercase_idx = Math.floor(Math.random() * lowercase_letters.length);
-            password_array.push(lowercase_letters[lowercase_idx]);
-        }
-        if (useSymbols) {
-            const symbols_idx = Math.floor(Math.random() * symbols.length);
-            password_array.push(symbols[symbols_idx]);
-        }
-
-        while (password_array.length < length_number.value) {
-            if (password_array.length < length_number.value) {
-                const number = Math.floor(Math.random() * 10);
-                password_array.push(number);
-            } 
-            if (useUppercase && password_array.length < length_number.value) {
-                const uppercase_idx = Math.floor(Math.random() * uppercase_letters.length);
-                password_array.push(uppercase_letters[uppercase_idx]);
-            }
-            if (useLowercase && password_array.length < length_number.value) {
-                const lowercase_idx = Math.floor(Math.random() * lowercase_letters.length);
-                password_array.push(lowercase_letters[lowercase_idx]);
-            }
-            if (useSymbols && password_array.length < length_number.value) {
-                const symbols_idx = Math.floor(Math.random() * symbols.length);
-                password_array.push(symbols[symbols_idx]);
-            }
-        }
+  if (!useUppercase && !useLowercase && !useSymbols) {
+    for (let i = 0; i < length_number.value; i++) {
+      const number = Math.floor(Math.random() * 10);
+      password_array.push(number);
+    }
+  } else {
+    if (useUppercase) {
+      const uppercase_idx = Math.floor(Math.random() * uppercase_letters.length);
+      password_array.push(uppercase_letters[uppercase_idx]);
+    }
+    if (useLowercase) {
+      const lowercase_idx = Math.floor(Math.random() * lowercase_letters.length);
+      password_array.push(lowercase_letters[lowercase_idx]);
+    }
+    if (useSymbols) {
+      const symbols_idx = Math.floor(Math.random() * symbols.length);
+      password_array.push(symbols[symbols_idx]);
     }
 
-    password_array.sort(() => Math.random() - 0.5);
-    input_box.value = password_array.join('');
+    while (password_array.length < length_number.value) {
+      if (password_array.length < length_number.value) {
+        const number = Math.floor(Math.random() * 10);
+        password_array.push(number);
+      }
+      if (useUppercase && password_array.length < length_number.value) {
+        const uppercase_idx = Math.floor(Math.random() * uppercase_letters.length);
+        password_array.push(uppercase_letters[uppercase_idx]);
+      }
+      if (useLowercase && password_array.length < length_number.value) {
+        const lowercase_idx = Math.floor(Math.random() * lowercase_letters.length);
+        password_array.push(lowercase_letters[lowercase_idx]);
+      }
+      if (useSymbols && password_array.length < length_number.value) {
+        const symbols_idx = Math.floor(Math.random() * symbols.length);
+        password_array.push(symbols[symbols_idx]);
+      }
+    }
+  }
+
+  password_array.sort(() => Math.random() - 0.5);
+  input_box.value = password_array.join("");
 }
 
-copy_button.addEventListener('click', function() {
+copy_button.addEventListener("click", function () {
+  if (input_box.value != "") {
     const temp_input = document.createElement("textarea");
     temp_input.value = input_box.value;
     document.body.appendChild(temp_input);
@@ -71,22 +75,37 @@ copy_button.addEventListener('click', function() {
     document.body.removeChild(temp_input);
 
     const notification = document.getElementById("pop_up");
-    notification.classList.add('show');
+    notification.classList.add("show");
     setTimeout(() => {
-        notification.classList.remove('show');
+      notification.classList.remove("show");
     }, 2000);
+  }
 });
 
-function next(){
-    if (length_number.value < 15){
-        const next_number= Number(length_number.value)+1
-        length_number.value=next_number
-    }
+function next() {
+  if (length_number.value < 15) {
+    const next_number = Number(length_number.value) + 1;
+    length_number.value = next_number;
+  } else {
+    const message = "Maximum password length is  15";
+    messageBox.innerHTML = message;
+    error.classList.add("show");
+    setTimeout(() => {
+      error.classList.remove("show");
+    }, 2000);
+  }
 }
 
-function previous(){
-    if (length_number.value > 6){
-        const next_number= Number(length_number.value)-1
-        length_number.value=next_number
-    }
+function previous() {
+  if (length_number.value > 6) {
+    const next_number = Number(length_number.value) - 1;
+    length_number.value = next_number;
+  } else {
+    const message = "Minimum password length is  6";
+    messageBox.innerHTML = message;
+    error.classList.add("show");
+    setTimeout(() => {
+      error.classList.remove("show");
+    }, 2000);
+  }
 }
